ee14b6bf36d7c29999966b7a5a26b6c2cba1c124
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2020-12-22 12:06:48 -0300

    Teste finalizaDO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import api from './services/api';
 
-const App: React.FC = () => <h1>Start</h1>;
+async function handleAddRepository(): Promise<void> {
+  const response = await api.post('/user/login', {
+    username: 'rafael',
+    password: 'testing123',
+  });
+}
+
+const App: React.FC = () => (
+  <button onClick={handleAddRepository}>Start</button>
+);
 
 export default App;
